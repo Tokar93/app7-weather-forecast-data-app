3039bd7c8c6f0518476d90a4ed796fe03cee51dc
tokarski-to <Tomasz.Tokarski@esk-sic.de>
2024-07-08 17:06:06 +0200

    Day 32: Exercise "Search for Happiness"

--- search-for-happiness/submain.py
+++ search-for-happiness/submain.py
@@ -9,10 +9,16 @@ cols_tuple = tuple(cols_format)
 
 cols_dict = {}
 for col_format, col in zip(cols_format, cols):
-    cols_dict[col_format] = col
+    if col == 'Gdp':
+        cols_dict[col_format] = col.upper()
+    else:
+        cols_dict[col_format] = col
 
 st.title('In Search for Happiness')
 
 x_axis = st.selectbox('Select the data for X-axis', options=cols_tuple)
 y_axis = st.selectbox('Select the data for Y-axis', options=cols_tuple)
 st.subheader(f'{x_axis} and {y_axis}')
+
+fig = px.scatter(x=df[cols_dict[x_axis]], y=df[cols_dict[y_axis]], labels={'x': cols_dict[x_axis], 'y': cols_dict[y_axis]})
+st.plotly_chart(fig)
